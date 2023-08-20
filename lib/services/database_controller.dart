ab742b7836aec6c4ee3a588d265bb653663055e7
Yesser <benselmayasser1@gmail.com>
2023-08-20 18:50:29 +0200

    Reduced redondance: infact instead of searching the database each time i could simply use the database variable i declared at the beginning

--- lib/services/database_controller.dart
+++ lib/services/database_controller.dart
@@ -7,22 +7,19 @@ class DatabaseController extends GetxController {
   var data = <String>[].obs;
   late Database database;
 
+  @override
   void onInit() {
     super.onInit();
     initializeDatabase();
   }
 
   Future<void> initializeDatabase() async {
-    // Get a reference to the database path
     String databasePath = await getDatabasesPath();
     String path = join(databasePath, 'my_database.db');
-
-    // Open the database or create it if it doesn't exist
-    Database database = await openDatabase(
+    database = await openDatabase(
       path,
       version: 1,
       onCreate: (db, version) async {
-        // Create the table
         await db.execute('''
         CREATE TABLE data (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,36 +31,20 @@ class DatabaseController extends GetxController {
   }
 
   Future<void> addTextToDatabase(String value) async {
-    // Get a reference to the database
-    String databasePath = await getDatabasesPath();
-    String path = join(databasePath, 'my_database.db');
-    Database database = await openDatabase(path);
-
-    // Insert the text into the 'data' table
     await database.insert(
-      'data', // Use the correct table name here
+      'data',
       {'text': value},
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-
   Future<void> printDatabaseContent() async {
-    // Get a reference to the database
-    String databasePath = await getDatabasesPath();
-    String path = join(databasePath, 'my_database.db');
-    Database database = await openDatabase(path);
-
-    // Query the database to retrieve all rows from the 'data' table
     List<Map<String, dynamic>> rows = await database.query('data');
 
-    // Print the content of each row
     for (var row in rows) {
       print('ID: ${row['id']}, Text: ${row['text']}');
     }
   }
-
-
 
   void save(String title, bool changeTitle, String value, bool changeData, int index, bool wasEmpty) async {
     if (index != -1) {
@@ -76,8 +57,8 @@ class DatabaseController extends GetxController {
     } else {
       titles.add(title);
       data.add(value);
-      addTextToDatabase(value);
-      printDatabaseContent();
+      await addTextToDatabase(value);
+      await printDatabaseContent();
     }
     update();
   }
